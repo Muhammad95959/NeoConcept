adb32a747ef4234b8fde04b65ce6e291c9ad2cf3
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2025-10-05 22:06:22 +0300

    add crud functions implementation for the subject rooms

--- backend/src/controllers/subjectsController.ts
+++ backend/src/controllers/subjectsController.ts
@@ -1,11 +1,86 @@
 import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
 
-export async function getRooms(req: Request, res: Response) {}
+const prisma = new PrismaClient();
 
-export async function getRoomById(req: Request, res: Response) {}
+export async function getRooms(req: Request, res: Response) {
+  const { search } = req.query;
+  try {
+    if (search) {
+      const rooms = await prisma.subjectRoom.findMany({
+        where: {
+          OR: [
+            { name: { contains: String(search), mode: "insensitive" } },
+            { description: { contains: String(search), mode: "insensitive" } },
+          ],
+        },
+      });
+      return res.status(200).json({ status: "success", data: rooms });
+    }
+    const rooms = await prisma.subjectRoom.findMany();
+    res.status(200).json({ status: "success", data: rooms });
+  } catch (err) {
+    console.log((err as Error).message);
+    res.status(500).json({ status: "fail", message: "Something went wrong" });
+  }
+}
 
-export async function createRoom(req: Request, res: Response) {}
+export async function getRoomById(req: Request, res: Response) {
+  const { id } = req.params;
+  try {
+    const room = await prisma.subjectRoom.findFirst({ where: { id: parseInt(id) } });
+    if (!room) return res.status(404).json({ status: "fail", message: "Room not found" });
+    res.status(200).json({ status: "success", data: room });
+  } catch (err) {
+    console.log((err as Error).message);
+    res.status(500).json({ status: "fail", message: "Something went wrong" });
+  }
+}
 
-export async function updateRoom(req: Request, res: Response) {}
+export async function createRoom(req: Request, res: Response) {
+  const { name, description } = req.body;
+  if (!name) return res.status(400).json({ status: "fail", message: "Subject room name is required" });
+  try {
+    const userRooms = await prisma.subjectRoom.findMany({ where: { createdBy: res.locals.user.id } });
+    if (userRooms.some((room) => room.name === name))
+      return res.status(400).json({ status: "fail", message: "Duplicate subject room name. Please choose another." });
+    const newRoom = await prisma.subjectRoom.create({ data: { name, description, createdBy: res.locals.user.id } });
+    res.status(201).json({ status: "success", data: newRoom });
+  } catch (err) {
+    console.log((err as Error).message);
+    res.status(500).json({ status: "fail", message: "Something went wrong" });
+  }
+}
 
-export async function deleteRoom(req: Request, res: Response) {}
+export async function updateRoom(req: Request, res: Response) {
+  const { id } = req.params;
+  try {
+    const room = await prisma.subjectRoom.findFirst({ where: { id: parseInt(id) } });
+    if (!room) return res.status(404).json({ status: "fail", message: "Room not found" });
+    const { name, description } = req.body;
+    const userRooms = await prisma.subjectRoom.findMany({ where: { createdBy: res.locals.user.id } });
+    if (userRooms.some((room) => room.name === name))
+      return res.status(400).json({ status: "fail", message: "Duplicate subject room name. Please choose another." });
+    const updatedRoom = await prisma.subjectRoom.update({
+      where: { id: parseInt(id) },
+      data: { name, description },
+    });
+    res.status(200).json({ status: "success", data: updatedRoom });
+  } catch (err) {
+    console.log((err as Error).message);
+    res.status(500).json({ status: "fail", message: "Something went wrong" });
+  }
+}
+
+export async function deleteRoom(req: Request, res: Response) {
+  const { id } = req.params;
+  try {
+    const room = await prisma.subjectRoom.findFirst({ where: { id: parseInt(id) } });
+    if (!room) return res.status(404).json({ status: "fail", message: "Room not found" });
+    await prisma.subjectRoom.delete({ where: { id: parseInt(id) } });
+    res.status(200).json({ status: "success", message: "Room deleted successfully" });
+  } catch (err) {
+    console.log((err as Error).message);
+    res.status(500).json({ status: "fail", message: "Something went wrong" });
+  }
+}
--- backend/src/routes/subjectsRoutes.ts
+++ backend/src/routes/subjectsRoutes.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import * as subjectsController from "../controllers/subjectsController";
+import * as authController from "../controllers/authController";
+import { Role } from "@prisma/client";
 
 const router = express.Router();
 
@@ -7,10 +9,25 @@ router.get("/get-rooms", subjectsController.getRooms);
 
 router.get("/get-room/:id", subjectsController.getRoomById);
 
-router.post("/create-room", subjectsController.createRoom);
+router.post(
+  "/create-room",
+  authController.protect,
+  authController.restrict(Role.INSTRUCTOR),
+  subjectsController.createRoom,
+);
 
-router.patch("/update-room/:id", subjectsController.updateRoom);
+router.patch(
+  "/update-room/:id",
+  authController.protect,
+  authController.restrict(Role.INSTRUCTOR),
+  subjectsController.updateRoom,
+);
 
-router.delete("/delete-room", subjectsController.deleteRoom);
+router.delete(
+  "/delete-room/:id",
+  authController.protect,
+  authController.restrict(Role.INSTRUCTOR),
+  subjectsController.deleteRoom,
+);
 
 export default router;
