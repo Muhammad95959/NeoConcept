6b286c3c895223136f333a436082122997e46e31
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2025-10-06 01:14:18 +0300

    add crud functions implementation for the posts

--- backend/src/controllers/postsController.ts
+++ backend/src/controllers/postsController.ts
@@ -1,11 +1,85 @@
 import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
 
-export async function getPosts(req: Request, res: Response) {}
+const prisma = new PrismaClient();
 
-export async function getPost(req: Request, res: Response) {}
+export async function getPosts(req: Request, res: Response) {
+  const { search } = req.query;
+  try {
+    if (search) {
+      const posts = await prisma.post.findMany({
+        where: {
+          subjectId: parseInt(res.locals.subjectId),
+          OR: [
+            { title: { contains: String(search), mode: "insensitive" } },
+            { content: { contains: String(search), mode: "insensitive" } },
+          ],
+        },
+      });
+      return res.status(200).json({ status: "success", data: posts });
+    }
+    const posts = await prisma.post.findMany({ where: { subjectId: parseInt(res.locals.subjectId) } });
+    res.status(200).json({ status: "success", data: posts });
+  } catch (err) {
+    console.log((err as Error).message);
+    res.status(500).json({ status: "fail", message: "Something went wrong" });
+  }
+}
 
-export async function createPost(req: Request, res: Response) {}
+export async function getPostById(req: Request, res: Response) {
+  const { id } = req.params;
+  try {
+    const post = await prisma.post.findUnique({ where: { subjectId: parseInt(res.locals.subjectId), id: parseInt(id) } });
+    if (!post) return res.status(404).json({ status: "fail", message: "Post not found" });
+    res.status(200).json({ status: "success", data: post });
+  } catch (err) {
+    console.log((err as Error).message);
+    res.status(500).json({ status: "fail", message: "Something went wrong" });
+  }
+}
 
-export async function updatePost(req: Request, res: Response) {}
+export async function createPost(req: Request, res: Response) {
+  const { title, content } = req.body;
+  try {
+    const newPost = await prisma.post.create({
+      data: { title, content, subjectId: parseInt(res.locals.subjectId), uploadedBy: res.locals.user.id },
+    });
+    res.status(201).json({ status: "success", data: newPost });
+  } catch (err) {
+    console.log((err as Error).message);
+    res.status(500).json({ status: "fail", message: "Something went wrong" });
+  }
+}
 
-export async function deletePost(req: Request, res: Response) {}
+export async function updatePost(req: Request, res: Response) {
+  const { id } = req.params;
+  try {
+    const post = await prisma.post.findUnique({ where: { subjectId: parseInt(res.locals.subjectId), id: parseInt(id) } });
+    if (!post) return res.status(404).json({ status: "fail", message: "Post not found" });
+    const { title, content } = req.body;
+    const updatedData: any = {};
+    if (title) updatedData.title = title;
+    if (content) updatedData.title = content;
+    const updatedPost = await prisma.post.update({
+      where: { subjectId: parseInt(res.locals.subjectId), id: parseInt(id) },
+      data: updatedData,
+    });
+    res.status(200).json({ status: "success", data: updatedPost });
+  } catch (err) {
+    console.log((err as Error).message);
+    res.status(500).json({ status: "fail", message: "Something went wrong" });
+  }
+}
+
+export async function deletePost(req: Request, res: Response) {
+  const { id } = req.params;
+  try {
+    const post = await prisma.post.findUnique({ where: { subjectId: parseInt(res.locals.subjectId), id: parseInt(id) } });
+    if (!post) return res.status(404).json({ status: "fail", message: "Post not found" });
+    await prisma.post.delete({ where: { subjectId: parseInt(res.locals.subjectId), id: parseInt(id) } });
+    res.status(200).json({ status: "success", message: "Post deleted successfully" });
+  } catch (err) {
+    console.log((err as Error).message);
+    res.status(500).json({ status: "fail", message: "Something went wrong" });
+  }
+}
--- backend/src/routes/postsRoutes.ts
+++ backend/src/routes/postsRoutes.ts
@@ -1,16 +1,39 @@
 import express from "express";
 import * as postsController from "../controllers/postsController";
+import * as authController from "../controllers/authController";
+import checkSubjectExists from "../middlewares/checkSubjectExists";
+import { Role } from "@prisma/client";
 
 const router = express.Router();
 
-router.get("/get-posts", postsController.getPosts);
+router.get("/get-posts/:subjectId", checkSubjectExists, postsController.getPosts);
 
-router.get("/get-post/:id", postsController.getPost);
+router.get("/get-post/:subjectId/:id", checkSubjectExists, postsController.getPostById);
 
-router.post("/create-post", postsController.createPost);
+router.post(
+  "/create-post/:subjectId",
+  authController.protect,
+  authController.restrict(Role.INSTRUCTOR),
+  checkSubjectExists,
+  postsController.createPost,
+);
 
-router.patch("/update-post/:id", postsController.updatePost);
+router.patch(
+  "/update-post/:subjectId/:id",
+  authController.protect,
+  authController.restrict(Role.INSTRUCTOR),
+  checkSubjectExists,
+  postsController.updatePost,
+);
 
-router.delete("/delete-post/:id", postsController.deletePost);
+router.delete(
+  "/delete-post/:subjectId/:id",
+  authController.protect,
+  authController.restrict(Role.INSTRUCTOR),
+  checkSubjectExists,
+  postsController.deletePost,
+);
 
 export default router;
+
+// TODO: these endpoints are ugly, do something like /api/v1/subjects/:subjectId/posts/:postId
