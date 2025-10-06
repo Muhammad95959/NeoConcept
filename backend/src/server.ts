ecd7ab0f34ea54d10d780bf92f44eb3852efdd56
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2025-10-06 19:48:17 +0300

    use cors in the server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import passport from "passport";
 import authRouter from "./routes/authRoutes";
 import subjectsRouter from "./routes/subjectsRoutes";
@@ -9,6 +10,7 @@ const app = express();
 const port = process.env.PORT || 9595;
 
 app.use(express.json());
+app.use(cors()); // TODO: configure CORS properly in production
 app.use(passport.initialize());
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/subjects", subjectsRouter);
