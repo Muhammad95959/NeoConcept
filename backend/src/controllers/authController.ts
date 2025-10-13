8e01fa905a7c3bea860f28b21895c955263a98b2
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2025-10-13 19:27:20 +0300

    grab the token from the authorization header or from the cookies

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -154,7 +154,12 @@ export async function resetPassword(req: Request, res: Response) {
 }
 
 export async function protect(req: Request, res: Response, next: NextFunction) {
-  const token = req.cookies.jwt;
+  let token: string | undefined;
+  if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+    token = req.headers.authorization.split(" ")[1];
+  } else if (req.cookies.jwt) {
+    token = req.cookies.jwt;
+  }
   if (!token) return res.status(401).json({ status: "fail", message: "You are not logged in" });
   try {
     if (!process.env.JWT_SECRET) throw new Error("JWT_SECRET is not defined");
