0a2cfdc21d175f86eb26c817b2d70fe99e994ea9
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2025-12-17 22:49:01 +0200

    update checkCourseExists function

--- backend/src/middlewares/checkCourseExists.ts
+++ backend/src/middlewares/checkCourseExists.ts
@@ -4,11 +4,11 @@ import prisma from "../config/db";
 export default async function checkCourseExists(req: Request, res: Response, next: Function) {
   const { courseId } = req.params;
   try {
-    const room = await prisma.course.findUnique({ where: { id: courseId } });
-    if (!room) return res.status(404).json({ status: "fail", message: "Room not found" });
+    const course = await prisma.course.findFirst({ where: { id: courseId, deletedAt: null } });
+    if (!course) return res.status(404).json({ status: "fail", message: "Course not found" });
     next();
   } catch (err) {
-    console.log((err as Error).message);
+    console.log(err);
     res.status(500).json({ status: "fail", message: "Something went wrong" });
   }
 }
